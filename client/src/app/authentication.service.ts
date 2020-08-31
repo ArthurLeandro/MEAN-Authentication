9922fc69c65b6635a975c7973286a64d11936157
ArthurLeandro <arthur.leandro11@gmail.com>
2020-08-30 22:28:39 -0300

    Few bugs fixed, at least the app is running on the view now

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators/map';
+import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 export interface UserDetails {
